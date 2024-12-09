35b084235bef9915cf1ba9c1eeca30e7a242004f
Chew Chee Ming <cheeming@gmail.com>
2024-12-09 17:39:43 +0800

    Add TF code to add HK server that have a public IP

--- aws-instances/main.tf
+++ aws-instances/main.tf
@@ -10,18 +10,101 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
+  region  = "ap-east-1"
 }
 
-resource "aws_instance" "awssg2" {
-  ami           = "ami-09b5c6390225b29cc"  // ubuntu 24.04 LTS arm64
-  instance_type = "t4g.nano"
-  associate_public_ip_address = true
-  key_name = "id_aws"
-  vpc_security_group_ids = ["sg-06517d58201cf854e"]
-  subnet_id = "subnet-2d9afc44"
+locals {
+  instance_name = "awshk1"
+}
+
+# networking
+
+resource "aws_vpc" "vpc_tailscale" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "awssg2"
+    Name = "vpc_tailscale"
+  }
+}
+
+resource "aws_subnet" "subnet_tailscale" {
+  vpc_id     = aws_vpc.vpc_tailscale.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "subnet_tailscale"
+  }
+}
+
+resource "aws_internet_gateway" "gw_tailscale" {
+  vpc_id = aws_vpc.vpc_tailscale.id
+
+  tags = {
+    Name = "gw_tailscale"
+  }
+}
+
+resource "aws_route_table" "rt_tailscale" {
+  vpc_id = aws_vpc.vpc_tailscale.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_tailscale.id
+  }
+
+  tags = {
+    Name = "rt_tailscale"
+  }
+}
+
+resource "aws_route_table_association" "rta_subnet_tailscale" {
+  subnet_id      = aws_subnet.subnet_tailscale.id
+  route_table_id = aws_route_table.rt_tailscale.id
+}
+
+
+# security
+
+resource "aws_security_group" "sg_only_ssh" {
+  name        = "sg_only_ssh"
+  description = "Allow SSH inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc_tailscale.id
+
+  tags = {
+    Name = "sg_only_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_in_ipv4_only_ssh" {
+  security_group_id = aws_security_group.sg_only_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_eg_ipv4_all" {
+  security_group_id = aws_security_group.sg_only_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_key_pair" "id_aws" {
+  key_name   = "id_aws"
+  public_key = var.public_key_id_aws
+}
+
+# servers
+
+resource "aws_instance" "ec2_server" {
+  ami           = "ami-096a8f4ba68bb4fdf"  // ubuntu 24.04 LTS arm64 in HK
+  instance_type = "t4g.nano"
+  associate_public_ip_address = true
+  key_name = aws_key_pair.id_aws.id
+  vpc_security_group_ids = [aws_security_group.sg_only_ssh.id]
+  subnet_id = aws_subnet.subnet_tailscale.id
+
+  tags = {
+    Name = local.instance_name
   }
 }
