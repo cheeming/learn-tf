d6396ccc450a121894f9417cc9e4c303d8986db4
Chew Chee Ming <cheeming@gmail.com>
2025-05-20 17:36:33 +0800

    Add resources for tmpsc.net
     - tmpsc.net will redirectt to www.tmpsc.net

--- www.cheeming.com/cloudfront.tf
+++ www.cheeming.com/cloudfront.tf
@@ -84,3 +84,75 @@ resource "aws_s3_bucket_website_configuration" "tmpsc_net_root" {
     host_name = "www.tmpsc.net"
   }
 }
+
+resource "aws_acm_certificate" "tmpsc_net_cert" {
+  domain_name       = "tmpsc.net"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  provider = aws.us-east-1
+}
+
+resource "aws_cloudfront_distribution" "tmpsc_net_cf" {
+
+  origin {
+    domain_name              = aws_s3_bucket_website_configuration.tmpsc_net_root.website_endpoint
+    origin_id                = local.s3_origin_id // ? need to double check if I need to change this.
+
+    custom_origin_config {
+      http_port               = 80
+      https_port              = 443
+      origin_protocol_policy  = "http-only"
+      origin_ssl_protocols    = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "To host tmpsc.net"
+  default_root_object = "index.html"
+
+  aliases = ["tmpsc.net"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type  = "none"
+      locations         = []
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.tmpsc_net_cert.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  depends_on = [
+    // cname validation needs to be complete for the ACM certificate,
+    // before CF can be created successfully
+    aws_route53_record.cname_validation_tmpsc_net_root
+  ]
+}
--- www.cheeming.com/dns.tf
+++ www.cheeming.com/dns.tf
@@ -8,8 +8,8 @@ resource "aws_route53_record" "alias_cf" {
   name    = "www.tmpsc.net"
   type    = "A"
   alias {
-    name = aws_cloudfront_distribution.www_cheeming_com_cf.domain_name
-    zone_id = aws_cloudfront_distribution.www_cheeming_com_cf.hosted_zone_id
+    name = aws_cloudfront_distribution.www_tmpsc_net_cf.domain_name
+    zone_id = aws_cloudfront_distribution.www_tmpsc_net_cf.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -31,15 +31,33 @@ resource "aws_route53_record" "cname_validation" {
   ttl     = 60
 }
 
-// create A record that points to the s3 bucket
+// create A record that points to the tmpsc.net cloudfront
 resource "aws_route53_record" "alias_cf_root" {
   zone_id = aws_route53_zone.tmpsc_net.zone_id
   name    = "tmpsc.net"
   type    = "A"
   alias {
-    name = aws_s3_bucket_website_configuration.tmpsc_net_root.website_domain
-    zone_id = aws_s3_bucket.tmpsc_net_root.hosted_zone_id
+    //name = aws_s3_bucket_website_configuration.tmpsc_net_root.website_domain
+    //zone_id = aws_s3_bucket.tmpsc_net_root.hosted_zone_id
+    name = aws_cloudfront_distribution.tmpsc_net_cf.domain_name
+    zone_id = aws_cloudfront_distribution.tmpsc_net_cf.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
+resource "aws_route53_record" "cname_validation_tmpsc_net_root" {
+  for_each = {
+    for dvo in aws_acm_certificate.tmpsc_net_cert.domain_validation_options:
+    dvo.domain_name => {
+      type = dvo.resource_record_type
+      name = dvo.resource_record_name
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.tmpsc_net.zone_id
+  type    = each.value.type
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+}
