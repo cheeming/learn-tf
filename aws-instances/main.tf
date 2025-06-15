12749bb0b01d0a5ccbae264bf4f0cd0867bc3a9f
Chew Chee Ming <cheeming@gmail.com>
2025-06-15 18:13:37 +0800

    Remove HongKong server

--- aws-instances/main.tf
+++ aws-instances/main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-module "tailscale_server_hongkong" {
-  source = "./modules/tailscale-exit-node"
-
-  aws_region        = "ap-east-1"
-  public_key_id_aws = var.public_key_id_aws
-  instance_name = "awshk1"
-  ami_id_ubuntu = "ami-096a8f4ba68bb4fdf"
-}
-
 module "tailscale_server_tokyo" {
   source = "./modules/tailscale-exit-node"
 
--- aws-instances/outputs.tf
+++ aws-instances/outputs.tf
@@ -1,13 +1,3 @@
-output "hk_instance_id" {
-  description = "ID of the EC2 instance"
-  value       = module.tailscale_server_hongkong.instance_id
-}
-
-output "hk_instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = module.tailscale_server_hongkong.instance_public_ip
-}
-
 output "jp_instance_id" {
   description = "ID of the EC2 instance"
   value       = module.tailscale_server_tokyo.instance_id
