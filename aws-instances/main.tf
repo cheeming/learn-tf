2210004ac1ff20e30e2201a104cb85923ef875f4
Chew Chee Ming <cheeming@gmail.com>
2025-06-15 18:41:18 +0800

    Add US server

--- aws-instances/main.tf
+++ aws-instances/main.tf
@@ -11,3 +11,13 @@ module "tailscale_server_tokyo" {
   ami_id_ubuntu = "ami-0329c152b4ffaa305"
   availability_zone = "ap-northeast-1b"
 }
+
+module "tailscale_server_us" {
+  source = "./modules/tailscale-exit-node"
+
+  aws_region        = "us-east-1"
+  public_key_id_aws = var.public_key_id_aws
+  instance_name = "awsus1"
+  ami_id_ubuntu = "ami-07041441b708acbd6"
+  availability_zone = "us-east-1a"
+}
--- aws-instances/outputs.tf
+++ aws-instances/outputs.tf
@@ -8,3 +8,13 @@ output "jp_instance_public_ip" {
   value       = module.tailscale_server_tokyo.instance_public_ip
 }
 
+output "us_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = module.tailscale_server_us.instance_id
+}
+
+output "us_instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = module.tailscale_server_us.instance_public_ip
+}
+
