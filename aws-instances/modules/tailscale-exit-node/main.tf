fadcbeb1dfe1217cf96327c5694e0a8866dd225f
Chew Chee Ming <cheeming@gmail.com>
2025-06-20 10:47:44 +0800

    Add 443/HTTPS port
     - This is to be paired with Websocket over HTTPS/TLS for shadowsocks

--- aws-instances/modules/tailscale-exit-node/main.tf
+++ aws-instances/modules/tailscale-exit-node/main.tf
@@ -69,6 +69,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_in_ipv4_only_ssh" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_in_ipv4_https" {
+  security_group_id = aws_security_group.sg_ssh_proxy_vpn.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_in_ipv4_only_shadowsocks_tcp" {
   security_group_id = aws_security_group.sg_ssh_proxy_vpn.id
   cidr_ipv4         = "0.0.0.0/0"
