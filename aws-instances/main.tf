10ffd9c7769dde2f9650aff219f85c429f681cfc
Chew Chee Ming <cheeming@gmail.com>
2025-08-02 16:28:59 +0800

    Remove US servers

--- aws-instances/main.tf
+++ aws-instances/main.tf
@@ -65,16 +65,6 @@ resource "aws_s3_object" "noip_env" {
   etag = md5(local.noip_env_content)
 }
 
-module "tailscale_server_us" {
-  source = "./modules/tailscale-exit-node"
-
-  aws_region        = "us-east-1"
-  public_key_id_aws = var.public_key_id_aws
-  instance_name     = "awsus1"
-  ami_id_ubuntu     = "ami-07041441b708acbd6"
-  availability_zone = "us-east-1a"
-}
-
 module "shadowsock_tokyo" {
   source = "./modules/tailscale-exit-node"
 
--- aws-instances/outputs.tf
+++ aws-instances/outputs.tf
@@ -8,12 +8,3 @@ output "jp_instance_public_ip" {
   value       = module.shadowsock_tokyo.instance_public_ip
 }
 
-output "us_instance_id" {
-  description = "ID of the EC2 instance"
-  value       = module.tailscale_server_us.instance_id
-}
-
-output "us_instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = module.tailscale_server_us.instance_public_ip
-}
